80b857a33e7ca9540774b1949249ee1cc4dfa216
umme aiman <122363026+umme-aimany@users.noreply.github.com>
2023-05-14 11:05:38 +0500

    Add files via upload

---
+++ java script/index.js
@@ -0,0 +1,21 @@
+alert("java script");
+let day=prompt("what is the day today",'');
+switch(day){
+    case "mon":
+    console.log("1 week day");
+    break;
+    case "tues":
+    console.log("2 week day");
+    break;
+    case "wed":
+    console.log("3 week day");
+    break;
+    case "sun":
+    console.log("holiday ");
+    break;
+    default:
+        console.log("enjoy")
+
+
+    
+}
